99af49fe491c98625c0bd90fe3e9ddf6a0204f7e
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-04-30 00:23:41 +0530

    Update updatebucket.sh

--- updatebucket.sh
+++ updatebucket.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #check if bucket exists and not empty
-gsutil -q stat gs://www.sps-cloud-rc/index.html
+gsutil -q stat gs://www.sps-cloud-rc.cloud/index.html
 
 return_value=$?
 
 if [[ $return_value == 0 ]]
 then
     echo "deleting existing FE folder"
-    gsutil rm -r gs://www.sps-cloud-rc
+    gsutil rm -r gs://www.sps-cloud-rc.cloud
 fi
 
-gsutil mb gs://www.sps-cloud-rc/
+gsutil mb gs://www.sps-cloud-rc.cloud/
 echo "uploading FE folder"
 
-gsutil cp -r ./* gs://www.sps-cloud-rc/
+gsutil cp -r ./* gs://www.sps-cloud-rc.cloud/
 
 #allow public acess to the bucket
-gsutil iam ch allUsers:objectViewer gs://www.sps-cloud-rc
+gsutil iam ch allUsers:objectViewer gs://www.sps-cloud-rc.cloud
 
 #set default static page for the bucket
-gsutil web set -m index.html -e 404.html gs://www.sps-cloud-rc
+gsutil web set -m index.html gs://www.sps-cloud-rc.cloud
