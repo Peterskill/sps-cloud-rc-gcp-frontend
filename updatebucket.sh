78666e6b8946e6902c142d20d0e11bd67236b883
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-04-30 14:33:05 +0530

    bug update

--- updatebucket.sh
+++ updatebucket.sh
@@ -5,16 +5,8 @@ gsutil -q stat gs://www.sps-cloud-rc.cloud/index.html
 
 return_value=$?
 
-if [[ $return_value == 0 ]]
-then
-    echo "deleting existing FE folder"
-    gsutil rm -r gs://www.sps-cloud-rc.cloud
-fi
 
-gsutil mb gs://www.sps-cloud-rc.cloud/
-echo "uploading FE folder"
-
-gsutil cp -r Website/* gs://www.sps-cloud-rc.cloud/
+gsutil rsync -d Website/* gs://www.sps-cloud-rc.cloud/
 
 #allow public acess to the bucket
 gsutil iam ch allUsers:objectViewer gs://www.sps-cloud-rc.cloud
