f4d23a7b0377bffad440466d638ce14286de4f5f
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-04-30 14:48:37 +0530

    new changes

--- updatebucket.sh
+++ updatebucket.sh
@@ -5,11 +5,23 @@ gsutil -q stat gs://www.sps-cloud-rc.cloud/index.html
 
 return_value=$?
 
+if [[ $return_value == 0 ]]
+then
+    echo "File exist"
+    gsutil rsync -d Website/* gs://www.sps-cloud-rc.cloud/
+    echo "all file sync done"
 
-gsutil rsync -d Website/* gs://www.sps-cloud-rc.cloud/
 
-#allow public acess to the bucket
-gsutil iam ch allUsers:objectViewer gs://www.sps-cloud-rc.cloud
+else
+    echo "creating new bucket"
+    gsutil mb -l us www.sps-cloud-rc.cloud -p cloud-resume-challenge-385006
+    echo "setting permission"
+    gsutil iam ch  allUsers:objectViewer gs://www.sps-cloud-rc.cloud
+    gsutil web set -m index.html gs://www.sps-cloud-rc.cloud
+fi
 
-#set default static page for the bucket
-gsutil web set -m index.html gs://www.sps-cloud-rc.cloud
+
+
+
+
+
