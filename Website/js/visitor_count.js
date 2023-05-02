34c0c4e75ad9044d4d2751111f52914cb8cabf97
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-05-02 18:47:25 +0530

    sps

--- Website/js/visitor_count.js
+++ Website/js/visitor_count.js
@@ -1,5 +1,5 @@
 
 
-fetch('https://us-central1-cloud-resume-challenge-385006.cloudfunctions.net/visitor_count').then(reponse=>reponse.json()).then(data=>{
+fetch('https://visitor-count-frk6a7sweq-uc.a.run.app').then(reponse=>reponse.json()).then(data=>{
     document.getElementById('visitor-count').innerText=data.count
 })
