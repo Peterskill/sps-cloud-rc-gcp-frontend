398b09940815c24a1abe031ee02f38d616050fda
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-05-13 19:39:17 +0530

    automation filr bug fix

--- updatebucket.sh
+++ updatebucket.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 #check if bucket exists and not empty
-gsutil -q stat gs://www.sps-cloud-rc.cloud/index.html
+gsutil -q stat gs://sps-cloud-rc.cloud/index.html
 
 return_value=$?
 
 if [[ $return_value == 0 ]]
 then
     echo "File exist"
-    gsutil rsync -d ./Website gs://www.sps-cloud-rc.cloud/
+    gsutil rsync -d ./Website gs://sps-cloud-rc.cloud/
     echo "all file sync done"
 
 
 else
     echo "creating new bucket"
-    gsutil mb -l us www.sps-cloud-rc.cloud -p cloud-resume-challenge-385006
+    gsutil mb -l us sps-cloud-rc.cloud -p cloud-resume-challenge-385006
     echo "syncing files"
-    gsutil rsync -d ./Website gs://www.sps-cloud-rc.cloud/
+    gsutil rsync -d ./Website gs://sps-cloud-rc.cloud/
     echo "Done sycning"
     echo "setting permission"
-    gsutil iam ch  allUsers:objectViewer gs://www.sps-cloud-rc.cloud
-    gsutil web set -m index.html gs://www.sps-cloud-rc.cloud
+    gsutil iam ch  allUsers:objectViewer gs://sps-cloud-rc.cloud
+    gsutil web set -m index.html gs://sps-cloud-rc.cloud
 fi
 
 
