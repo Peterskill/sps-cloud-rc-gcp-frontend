64c514119134719e64053782410dc105957fd0f9
Peterskill <104547222+Peterskill@users.noreply.github.com>
2023-09-17 15:29:18 +0530

    Update visitor_count.js

--- Website/js/visitor_count.js
+++ Website/js/visitor_count.js
@@ -1,5 +1,5 @@
 
 
-fetch('https://us-central1-sps-cloud-rc.cloudfunctions.net/visitor-count-v1').then(reponse=>reponse.json()).then(data=>{
+fetch('https://us-central1-sps-cloud-rc.cloudfunctions.net/viscountgcp').then(reponse=>reponse.json()).then(data=>{
     document.getElementById('visitor-count').innerText=data.count
 })
